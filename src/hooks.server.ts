c55bf41c48fec63a57156b14df042a072aecc31a
Luke Thorne <the.thorne48@gmail.com>
2025-03-15 12:33:22 -0500

    allow unauthorized sign up/in

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -37,7 +37,7 @@ const authGuard: Handle = async ({ event, resolve }) => {
   event.locals.user = user
 
   // if they aren't logged in, redirect to the homepage
-  if (!user && event.url.pathname !== "/" && !event.url.pathname.startsWith("/twitch") && !event.url.pathname.startsWith("/priming")) {
+  if (!user && event.url.pathname !== "/" && !event.url.pathname.startsWith("/twitch") && !event.url.pathname.startsWith("/priming") && !event.url.pathname.startsWith("/auth/signup") && !event.url.pathname.startsWith("/auth/login")) {
     redirect(303, '/')
   }
 
